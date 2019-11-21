15091feff63dc8f89186f457110a766704182432
Admin <you@example.com>
2019-11-21 11:53:01 +0530

    Add:regex for emailaddress

--- registration.sh
+++ registration.sh
@@ -19,3 +19,13 @@ then
 else
 	echo "Invalid Lastname"
 fi
+echo "Enter the valid Email Address"
+read email
+emailRegex="^[a-zA-Z]{1,}([._-+]{1}?[a-zA-Z0-9]+)?[@]{1}[a-zA-Z0-9]{1,}([.]{1}[a-zA-Z]{2,4})?[.]{1}*[a-zA-Z]{2,4}*$"
+if [[ $email =~ $emailRegex ]]
+then
+	echo "Valid Email address"
+else
+	echo "Invalid Email address"
+	echo "eg.abc.xyz@bl.co.in Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions"
+fi

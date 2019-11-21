20b92c5f18feffbb20927e9492646937d6164855
Admin <you@example.com>
2019-11-21 11:45:10 +0530

    Add:regex for firstname should start with uppercase and contain minimum 3 letters

--- registration.sh
+++ registration.sh
@@ -2,3 +2,12 @@
 
 echo "WEL-COME"
 
+echo "Enter the First Name"
+read firstName
+nameRegex="^[A-Z]{1}[a-z]{2,}$"
+if [[ $firstName =~ $nameRegex ]]
+then
+	echo "Valid Firstname"
+else
+	echo "Invalid Fisrtname"
+fi
